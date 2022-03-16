ef996ac04763b3cd4d25480c692a8874e0150643
magnetenstad <tenstad.magne@gmail.com>
2022-03-16 15:43:27 +0100

    Add ask_select

--- db2/src/utils.py
+++ db2/src/utils.py
@@ -1,10 +1,29 @@
 
 def ask(questions, types=None):
-	try:
-		if types == None:
-			return [input(f'{q}: ') for q in questions]
-		else:
-			return [types[i](input(f'<{types[i].__name__}> {q}: ')) for i, q in enumerate(questions)]
-	except Exception as e:
-		print(f'ERROR - {e}')
+  if types == None:
+    return [input(f'{q}: ') for q in questions]
+  else:
+    answers = []
+    for i, q in enumerate(questions):
+      while True:
+        try:
+          answers.append(
+            types[i](input(f'<{types[i].__name__}> {q}: ').lower()))
+        except:
+          print('Ugyldig input!')
+
+def ask_select(question, options, indexed=False):
+  if indexed:
+    options = [f'({i})' + option for i, option in enumerate(options)]
+    # question += '\n(skriv tallet)'
+  options_str = '\t' + '\n\t'.join(options)
+  while True:
+    selected = input(f'{question}\n{options_str}\n').lower()
+    if selected in options:
+      return selected
+    try:
+      return options[int(selected)]
+    except:
+      pass
+    print('Ugyldig input!')
 
